5ba9b8a296cea3b305fefdd655b6672d9e4f0503
Rachael McVay <rachaelmcvay@Rachaels-MacBook-Pro.local>
2025-01-05 14:27:39 -0500

    adding starting project

---
+++ script.js
@@ -0,0 +1,38 @@
+// var css = document.querySelector('h3');
+// var color1 = document.querySelector('.color1');
+// var color2 = document.querySelector('.color2');
+// var body = document.getElementById('gradient');
+
+// function setGradient() {
+// 	body.style.background = 'linear-gradient(to right, ' + color1.value + ' , ' + color2.value + ')';
+// 	css.textContent = body.style.background + ';'
+// }
+
+// color1.addEventListener('input', setGradient);
+
+// color2.addEventListener('input', setGradient);
+
+const css = document.querySelector('h3');
+const [color1, color2] = document.querySelectorAll('.color')
+const body = document.querySelector('#gradient');
+
+setGradient = () => {
+	body.style.background = _linearGradient();
+	css.textContent = `${body.style.background};`;
+}
+
+
+_linearGradient = () => {
+	return `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
+
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
+
+// const flattened = [[0,1], [2,3], [4,5]].reduce(
+// 	(accumulator, array) => {
+// 		console.log(‘array’, array);
+// 		console.log(‘accumulator’, accumulator);
+// 		return accumulator.concat(array)
+// }, []);
