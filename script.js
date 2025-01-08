ce1fad8a27e6f9c8562ba40ece3fb1af5cede237
Rachael McVay <rmcvay@proton.me>
2025-01-08 00:52:10 -0500

    tweaks with browserify

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 
 // color2.addEventListener('input', setGradient);
 
-var _ = require('lodash');
+var _ = require("lodash");
 
 var array = [1,2,3,4,5,6,7,8];
 console.log('answer: ', _without(array, 3));
